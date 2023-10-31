74b17dcdcc1b245fce6d90a85b2fb229e5be0d7e
Asaad Shaikh <91770135+asaadshaikh@users.noreply.github.com>
2023-10-31 21:50:32 +0530

    Add files via upload

---
+++ FirstProgram.py
@@ -0,0 +1,10 @@
+price=int(input("Enter the price of a donut: Rs. "))
+quantity=int(input("Enter the no. of Donuts: "))
+amount=price*quantity
+
+
+if amount>1000
+    price ("Yayy..a discount of 10% is applicable!")
+    discount=amount*10/1000
+    amount=amount-discount
+print("Your total amount is: Rs. ",amount)
